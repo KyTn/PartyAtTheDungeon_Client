bcba8d2dabb5a8e8a92d5e1d9f49939c94971c32
KyTn <angel.kytn@gmail.com>
2017-01-25 20:29:45 +0100

    build.cs

--- Source/PATD_Client/PATD_Client.Build.cs
+++ Source/PATD_Client/PATD_Client.Build.cs
@@ -6,7 +6,7 @@ public class PATD_Client : ModuleRules
 {
 	public PATD_Client(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Networking", "Sockets"  });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
